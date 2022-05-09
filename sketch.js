fa0e7238068ba7d3e49703f2aa1ddd540e4353d4
betamoreira <79289691+betamoreira@users.noreply.github.com>
2022-05-09 19:46:36 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,1 +1,29 @@
-var trex ,trex_correndo;var solo ,solo_image;function preload(){trex_correndo = loadAnimation("trex1.png","trex3.png","trex4.png");solo_image = loadImage("ground2.png");}function setup(){createCanvas(600,200);trex = createSprite(30,160,20,50);trex.addAnimation("corrida",trex_correndo);trex.scale = 0.5;solo = createSprite(300,180,600,20);solo.addImage(solo_image);solo.x = solo.width/2;}function draw(){background(220);solo.velocityX = -2;console.log(solo.x);if(solo.x<0){  solo.x = solo.width/2;}if(keyDown("space")){trex.velocityY = -10;}trex.velocityY = trex.velocityY + 0.5;trex.collide(solo);drawSprites();}
+var trex, trex_running, trex_collided;
+var ground, invisibleGround, groundImage;
+function preload() {
+trex_running = loadAnimation("trex1.png", "trex3.png", "trex4.png");
+trex_collided = loadImage("trex_collided.png");
+groundImage = loadImage("ground2.png")
+}
+function setup(){
+createCanvas(600, 200);
+trex = createSprite(50,160,20,50);
+trex.addAnimation("running", trex_running);
+trex.scale = 0.5;
+ground = createSprite(200,180,400,20);
+ground.addImage("ground",groundImage);
+ground.x = ground.width /2;
+ground.velocityX = -4;
+}
+function draw() {
+background(220);
+if(keyDown("space")){
+trex.velocityY = -10;
+}
+trex.velocityY = trex.velocityY + 0.8
+if(ground.x < 0){
+ ground.x = ground.width / 2;
+}
+trex.collide(ground);
+drawSprites();
+}
